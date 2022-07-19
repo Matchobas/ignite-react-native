119b6fec8794e694cbaadae9fbdcbba11440b20f
Matchobas <maalexandria@hotmail.com>
2022-07-18 23:49:35 -0300

    Starting to create my own screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Matheus</Text>
+      </View>
+      <Text style={{ fontWeight: 'bold', color: '#FF00FF', alignSelf: 'center', marginBottom: 20}}>Ignite</Text>
+    </>
+  )
+}
